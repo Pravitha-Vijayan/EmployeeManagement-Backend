1acc03476193411689432dcc414c8481f5780901
Pravitha Vijayan <96038738+Pravitha-Vijayan@users.noreply.github.com>
2023-09-03 19:08:12 +0530

    search and sort added and image colum also added

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,61 +1,78 @@
 package net.javaguides.springboot.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	@Column(name = "first_name")
+
+	@Column(name = "first_name", nullable = false) // Assuming first name is required if not required you can make it nullable true
 	private String firstName;
 
-	@Column(name = "last_name")
+	@Column(name = "last_name", nullable = false) // Assuming last name is required
 	private String lastName;
-	
-	@Column(name = "email_id")
+
+	@Column(name = "email_id", unique = true, nullable = false) // Assuming email is unique and required
 	private String emailId;
-	
+
+	@Lob // For large binary data like images
+	@Column(name = "image")
+	private byte[] image;
+
+	// Default constructor is required by JPA, you can add it
 	public Employee() {
-		
 	}
-	
-	public Employee(String firstName, String lastName, String emailId) {
-		super();
+
+	public Employee(String firstName, String lastName, String emailId, byte[] image) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailId = emailId;
+		this.image = image;
 	}
+
+	// Getters and setters
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
+//	public byte[] getImage() {
+//		return image;
+//	}
+//
+//	public void setImage(byte[] image) {
+//		this.image = image;
+//	}
 }
--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,10 +1,26 @@
 package net.javaguides.springboot.repository;
 
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.model.QEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, QuerydslPredicateExecutor<Employee>, QuerydslBinderCustomizer<QEmployee> {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QEmployee root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.id);
+        bindings.including(root.firstName);
+        bindings.including(root.lastName);
+        bindings.including(root.emailId);
+        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 }
